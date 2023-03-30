7458fb6f66960f9f8db64047224adac839a5b8ff
Ivan Yuan <ivan2100736@gmail.com>
2023-03-31 01:42:35 +0800

    style init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+import '@/assets/scss/all.scss';
 
 createApp(App).use(store).use(router).mount('#app');
