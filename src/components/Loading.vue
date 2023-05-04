5f7501a3a526ff57ae14692cf0bf87dc3656aca6
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2023-05-04 21:23:41 +0800

    feat: loading 元件

---
+++ src/components/Loading.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="loading">
+    <div class="loading__body bg-dark-100">
+      <svg class="spinner" viewBox="0 0 50 50">
+        <circle
+          class="path"
+          cx="25"
+          cy="25"
+          r="20"
+          fill="none"
+          stroke-width="5"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.loading{
+  @apply flex justify-center absolute w-full;
+}
+.spinner {
+  @apply w-8 h-8;
+  animation: rotate 2s linear infinite;
+  & .path {
+    stroke: #1e40af;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+  }
+}
+
+@keyframes rotate {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 150;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 90, 150;
+    stroke-dashoffset: -124;
+  }
+}
+</style>
