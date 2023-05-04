f5f9cf7e47fd83fd0426e8b52041ddaec42c904c
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2023-05-04 21:23:21 +0800

    feat: 建立結帳清單元件

---
+++ src/components/cart/shoppingItem.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  orderList: Array,
+});
+</script>
+<template>
+  <div>
+
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/cart/shoppingList.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { defineProps, PropType } from 'vue';
+import { IShoppingItem } from '../../../entities';
+
+defineProps({
+  shoppingList: {
+    type: Array as PropType<IShoppingItem[]>,
+    default: () => [],
+  },
+});
+</script>
+<template>
+  <div>
+
+  </div>
+</template>
+<style lang="scss" scoped>
+
+</style>
