d42b4fa8a87da432e4dea6e80cf2b0d167c079f4
Ivan Yuan <ivan2100736@gmail.com>
2023-05-30 07:54:25 +0800

    feat: check list

--- src/components/cart/shoppingItem.vue
+++ src/components/cart/shoppingItem.vue
@@ -1,14 +1,33 @@
-<script lang="ts" setup>
-import { defineProps } from 'vue';
+<script lang="ts">
+import { defineComponent } from 'vue';
 
-defineProps({
-  orderList: Array,
+export default defineComponent({
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
 });
 </script>
 <template>
-  <div>
-
-  </div>
+  <tr>
+    <td align="center" width="25%">{{ item['product_name'] }}</td>
+    <td align="center" width="15%">{{ item['price_per_unit'] }}</td>
+    <td align="center" width="13%">{{ item.buyCount }}</td>
+    <td align="center" width="12%">
+      <div contenteditable="true">{{ item['price_per_unit'] }}</div>
+    </td>
+    <td align="center" width="15%">
+      <div contenteditable="true" @keyup.enter.prevent>100</div>
+    </td>
+    <td align="center" width="20%">{{ 300 * 2 * 0.8 - 100 }}</td>
+  </tr>
 </template>
 <style lang="scss" scoped>
 
--- src/components/cart/shoppingList.vue
+++ src/components/cart/shoppingList.vue
@@ -10,10 +10,23 @@ defineProps({
 });
 </script>
 <template>
-  <div>
-
-  </div>
+  <table class="table table-fixed w-full">
+    <thead class="thead w-full">
+      <tr class="w-full">
+        <th class="p-1" width="25%">名稱</th>
+        <th class="p-1" width="15%">售價</th>
+        <th class="p-1" width="13%">數量</th>
+        <th class="p-1" width="12%">打折</th>
+        <th class="p-1" width="15%">折扣金額</th>
+        <th class="p-1" width="20%">小結</th>
+      </tr>
+    </thead>
+    <tbody>
+      <template v-for="item in shoppingList" :key="item.id">
+          <ShoppingItem :item="item" />
+      </template>
+    </tbody>
+  </table>
 </template>
 <style lang="scss" scoped>
-
 </style>
