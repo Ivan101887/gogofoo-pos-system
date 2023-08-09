766c5b0ecf346f533921f690b00034e4897a51c3
Ivan Yuan <ivan2100736@gmail.com>
2023-08-10 00:43:38 +0800

    config: ignore eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/multi-word-component-names': 'off',
     'vuejs-accessibility/click-events-have-key-events': 'off',
+    'no-alert': 0,
   },
 };
