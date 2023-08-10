ba057734a8ce06495a03f134247f79ad25dec779
Ivan Yuan <ivan2100736@gmail.com>
2023-08-11 03:07:45 +0800

    feat: 鍵盤移除不需要的操作鍵

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,25 +1,18 @@
 <template>
   <div class="calculator" @keyup.delete.capture="backspace">
     <div class="calculator__body">
-      <input type="button" value="清除" class="btn" @click="clear">
-      <input type="button" value="CE" class="btn" @click="clear">
-      <input type="button" value="更正" class="btn" @click="backspace">
-      <input type="button" value="/" class="btn">
       <input type="button" value="7" class="btn" @click="appendNumber('7')">
       <input type="button" value="8" class="btn" @click="appendNumber('8')">
       <input type="button" value="9" class="btn" @click="appendNumber('9')">
-      <input type="button" value="*" class="btn">
       <input type="button" value="4" class="btn" @click="appendNumber('4')">
       <input type="button" value="5" class="btn" @click="appendNumber('5')">
       <input type="button" value="6" class="btn" @click="appendNumber('6')">
-      <input type="button" value="-" class="btn">
       <input type="button" value="1" class="btn" @click="appendNumber('1')">
       <input type="button" value="2" class="btn" @click="appendNumber('2')">
       <input type="button" value="3" class="btn" @click="appendNumber('3')">
-      <input type="button" value="+" class="btn">
+      <input type="button" value="清除" class="btn" @click="clear">
+      <input type="button" value="更正" class="btn" @click="backspace">
       <input type="button" value="0" class="btn" @click="appendNumber('0')">
-      <input type="button" value="." class="btn">
-      <input type="button" value="結帳" class="btn btn__lg">
     </div>
   </div>
 </template>
@@ -76,23 +69,15 @@ export default defineComponent({
     @apply bg-white text-right p-3 text-3xl;
   }
   &__body{
-    @apply grid grid-cols-4 h-full;
+    @apply grid grid-cols-3 h-full;
   }
 }
 
-.btn,
-.btn__lg{
+.btn{
   @apply text-white rounded-none border-0;
   background-color: #3598da;
 }
-.btn__lg{
-  @apply col-span-2;
-  background-color: #2891d8;
-}
 .btn:hover{
   background-color: #268ccf;
-}
-.btn__lg:hover{
-  background-color: #2483c3;
 }
 </style>
